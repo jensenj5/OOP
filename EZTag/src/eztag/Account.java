ebaa10b4cb96305a37df2ca9f1c21bc9983c408f
jensenj5 <jensenj5@gator.uhd.edu>
2016-03-24 11:04:06 -0500

    Created project

---
+++ EZTag/src/eztag/Account.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eztag;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author jjred88
+ */
+public class Account {
+    
+}
---
+++ EZTag/src/eztag/EZTag.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eztag;
+
+/**
+ *
+ * @author jjred88
+ */
+
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+
+public class EZTag {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VirtualScanner v = new VirtualScanner();
+        //VirtualScannerUI vu = new VirtualScannerUI();
+        new VirtualScannerUI().setVisible(true);
+    }
+    
+}
